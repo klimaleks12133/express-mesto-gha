bcc01931960b7c2c1d15669a6bd25e2a51e779a3
klimaleks12133 <120417478+klimaleks12133@users.noreply.github.com>
2024-01-29 16:04:22 +0300

    refaactor

--- models/users.js
+++ models/users.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const validator = require('validator');
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -34,4 +36,22 @@ const userSchema = new mongoose.Schema({
   },
 });
 userSchema.set('versionKey', false);
+
+userSchema.statics.findUserByCredentials = function auth(email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+          return user;
+        });
+    });
+};
+
 module.exports = mongoose.model('user', userSchema);
